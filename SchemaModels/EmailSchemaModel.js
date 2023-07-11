e770b49620190336fff5718dee7ee6a958b0e4fb
Avula Sai Raghu Kiran <avulasairaghukiran1998@gmail.com>
2023-07-11 18:54:17 +0530

    new commit

--- SchemaModels/EmailSchemaModel.js
+++ SchemaModels/EmailSchemaModel.js
@@ -15,7 +15,7 @@ const sent_email_schema = new mongoose.Schema({
   },
   created_ByUser: {
     type: "string",
-    required: true,
+    required: false,
   },
   email_list: {
     type: "string",
